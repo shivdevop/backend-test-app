f90bd3f91f46f99dc71c93d136f9f6829ce0cc29
shivam <shivamagarwalla05@gmail.com>
2025-06-28 18:02:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// script mei we can write commands that we can run 
+const express=require('express');
+const app=express();
+
+require('dotenv').config();
+const port=process.env.port || 3000
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
